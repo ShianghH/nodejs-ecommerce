07474b79abcba6b46b72d3cda141e27cc87ca586
shiang <a300251992@gmail.com>
2025-11-28 16:45:31 +0800

    建立seed&seed-reset

---
+++ scripts/seed.js
@@ -0,0 +1,7 @@
+require("dotenv/config");
+
+const dataSource = require("../db/data-source");
+
+const User = require("../entities/User");
+const UserRole = require("../entities/UserRole");
+const ProductImage = require("../entities/ProductImage");
