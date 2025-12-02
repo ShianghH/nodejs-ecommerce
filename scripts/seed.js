0fbb4203b26d142649b0a3e015a642634b8616b9
shiang <a300251992@gmail.com>
2025-12-02 17:12:42 +0800

    seed.js ---

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,7 +1,36 @@
 require("dotenv/config");
 
-const dataSource = require("../db/data-source");
+const { dataSource } = require("../db/data-source");
 
 const User = require("../entities/User");
 const UserRole = require("../entities/UserRole");
+const Products = require("../entities/Product");
+const ProductCategory = require("../entities/ProductCategory");
+const ProductVariant = require("../entities/ProductVariant");
 const ProductImage = require("../entities/ProductImage");
+const ProductTag = require("../entities/ProductTag");
+const Tag = require("../entities/Tag");
+const OrderItem = require("../entities/OrderItem");
+
+const bcrypt = require("bcrypt");
+
+const seed = async () => {
+  console.log("\n[seed] 開始執行（不清空資料，只補缺的）...\n");
+  await dataSource.initialize();
+
+  const userRepo = dataSource.getRepository(User);
+  const userRoleRepo = dataSource.getRepository(UserRole);
+  const productRepo = dataSource.getRepository(Products);
+  const categoryRepo = dataSource.getRepository(ProductCategory);
+  const variantRepo = dataSource.getRepository(ProductVariant);
+  const imageRepo = dataSource.getRepository(ProductImage);
+  const productTagRepo = dataSource.getRepository(ProductTag);
+  const tagRepo = dataSource.getRepository(Tag);
+  const orderRepo = dataSource.getRepository(OrderItem);
+
+  //1.管理員帳號
+  const adminEmail = "adminVip@example.com";
+  let admin = await userRoleRepo.findOne({
+    where: { name: "Admin" },
+  });
+};
