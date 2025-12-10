02411b97b15d2251b3249517f4da57b64074c05e
shiang <a300251992@gmail.com>
2025-12-10 16:50:44 +0800

    seed.js--demoName

--- scripts/seed.js
+++ scripts/seed.js
@@ -30,7 +30,58 @@ const seed = async () => {
 
   //1.管理員帳號
   const adminEmail = "adminVip@example.com";
-  let admin = await userRoleRepo.findOne({
-    where: { name: "Admin" },
+  let admin = await userRepo.findOne({
+    where: { email: adminEmail },
   });
+  if (!admin) {
+    const passwordHash = await bcrypt.hash("admin", 10);
+
+    admin = await userRepo.save(
+      userRepo.create({
+        name: "Admin",
+        email: adminEmail,
+        password: passwordHash,
+      })
+    );
+    console.log("[seed]管理員建立成功", adminEmail);
+  } else {
+    console.log("[seed]管理員已存在，跳過", adminEmail);
+  }
+
+  //2.分類
+  let defaultCategory = await categoryRepo.findOne({
+    where: { name: "Default" },
+  });
+  if (!defaultCategory) {
+    defaultCategory = await categoryRepo.save(
+      categoryRepo.create({
+        name: "Default",
+      })
+    );
+    console.log("[seed]建立分類: Default");
+  } else {
+    console.log("[seed]分類Default已存在,跳過");
+  }
+
+  //3.商品+圖片
+
+  const demoName = "Demo Keyboard";
+  let product = await productRepo.findOne({
+    where: {
+      name: demoName,
+    },
+    relations: ["category", "images", "variants"],
+  });
+
+  if (!product) {
+    product = await productRepo.save(
+      productRepo.create({
+        name: demoName,
+        price: 1990,
+        discount_price: 1500,
+        is_active: true,
+        category: defaultCategory,
+      })
+    );
+  }
 };
